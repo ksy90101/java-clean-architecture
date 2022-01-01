f78e72b1d60ed695fe7c8cc83ad7a9ea65de5c1d
ksy90101 <ksy90101@gmail.com>
2022-01-01 14:10:30 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/com/book/cleanarchitecture/JavaCleanArchitectureApplication.java
@@ -0,0 +1,13 @@
+package com.book.cleanarchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCleanArchitectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaCleanArchitectureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/book/cleanarchitecture/JavaCleanArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.book.cleanarchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCleanArchitectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
